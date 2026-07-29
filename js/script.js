a46d8aae3c8df2a02efda9e15e83e917abda4543
ambedkariyajanataparty <myemail@gmail.com>
2026-07-29 18:19:30 +0530

    Added social media links to website header

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const menuBtn = document.getElementById("menu-btn");
+    const nav = document.getElementById("nav");
+
+    if(menuBtn && nav){
+
+        menuBtn.addEventListener("click", function(){
+
+            nav.classList.toggle("show");
+
+        });
+
+    }
+
+});
